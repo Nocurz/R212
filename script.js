5db08c8f028e3e51133d3dc612ae36aced6a8562
Nocurz <carl.drouet@etu.umontpellier.fr>
2024-03-18 15:43:51 +0100

    Changer toute l'interface de l'application

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
-import file from "./data.json" assert { type: "json" };
-console.log(file.version);
+$(document).ready(function() {
+  var jsonFile = 'data.json';
 
-const module = await import("./data.json", {
-  with: { type: "json" },
+  // Function to fetch JSON data
+  function fetchData() {
+    $.getJSON(jsonFile, function(data) {
+      // Iterate through JSON data and create Bootstrap cards
+      $.each(data, function(index, item) {
+        var cardHTML = `
+          <div class="col-md-4 mb-4 col-lg-3">
+            <div class="card">
+              <img src="${item.album.images[1].url}" class="card-img-top" alt="${item.title}">
+              <div class="card-body" style="height: 200px;">
+                <div style="height: 100%; display: flex; flex-direction: column; justify-content: space-between;">
+                  <div>
+                    <h4 class="card-title">${item.name}</h4>
+                    <p>
+                      ${item.artists.map((artist) => artist.name).join(", ")}
+                    </p>
+                  </div>
+                  <div>
+                    <button class="btn btn-primary read-btn" data-playing="false" data-audio="${item.preview_url}">Play</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        `;
+        // Append the card HTML to the card container
+        $('#card-container').append(cardHTML);
+      });
+    });
+  }
+
+  // Call fetchData function to load JSON data and create cards
+  fetchData();
+
+  function playMusic(audioPath) {
+      var audio = new Audio(audioPath);
+      audio.play();
+  }
+
+  
+
 });
-console.log(module.default.version);
+
+
+
+
